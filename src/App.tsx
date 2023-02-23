884a81682efb9b0f12b57a56c2dfcfd6ca5d56b6
Ilya Kudinkin <kudincikin@gmail.com>
2023-02-24 01:11:38 +0300

    сделано все, но без адаптивной верстки

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,195 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import image1 from './images/Image1.png';
+import image2 from './images/Image2.png';
+import image3 from './images/Image3.png';
+import image4 from './images/Image4.png';
+import image5 from './images/Image5.png';
+import image6 from './images/Image6.png';
+import bootstrap from './images/bootstrap.svg';
+import html5 from './images/html5.svg';
+import rickroll from './videos/rickroll.mp4';
+import { useRef, useState } from 'react';
+import { SendForm } from './components/SendForm';
 
 function App() {
+
+  const videoRef = useRef(null);
+  const [playOpacity, setPlayOpacity] = useState(1);
+  const [formVisibility, setFormVisibility] = useState(false);
+  const [time, setTime] = useState(false);//false - месяц, true - год
+
+  function playpause(){
+    const video = videoRef.current;
+    if (videoRef.current === null)
+      return;
+    if (video.paused || video.ended) {
+      videoRef.current.play();
+      setPlayOpacity(0);
+    } else {
+      videoRef.current.pause();
+      setPlayOpacity(1);
+    }
+  };
+
+  function showForm(){
+    setFormVisibility(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+
+      <header className="header">
+        <nav className='header-nav'>
+          <div>Изображения</div>
+          <div>Видео</div>
+          <div>3D модели</div>
+          <div className='round-button pink-button'>Войти</div>
+        </nav>
       </header>
+
+      <main className='main'>
+        <div className='greeting-block'>
+          <div className='greeting-text'>
+            <h2 className='greeting-description'>
+              Теперь у вас под рукой всегда есть нужные изображения, видео, музыка, 
+              3D-модели, редакционные материалы и авторские продукты. 
+            </h2>
+            <h1 className='greeting'>Превратите свои идеи в реальные достижения</h1>
+          </div>
+        </div>
+
+        <div className='gallery-container'>
+          <div className='gallery'>
+            <img src={image1} className="image" alt='img1'/>
+            <img src={image2} className="image" alt='img2'/>
+            <img src={image3} className="image" alt='img3'/>
+            <img src={image4} className="image" alt='img4'/>
+            <img src={image5} className="image" alt='img5'/>
+            <img src={image6} className="image" alt='img6'/>
+          </div>
+        </div>
+
+        <div className='video-gallery'>
+          <h2 className='video-header'>
+            Поможем сделать ваш продукт уникальным
+          </h2>
+          <div className='video-player'>
+            <video 
+                ref={videoRef}
+                id='video'
+                className="tmp-vid"
+                src={rickroll}
+                onClick={playpause}
+            ></video>
+            <button id='playpause' style={{opacity: `${playOpacity}`}} onClick={playpause}><div className='play-button'/></button>
+          </div>
+          <nav className='video-nav'>
+            <div className='video-nav-element'>
+              <div className='img-video-nav'>
+                <div className='play-button'/>
+              </div>
+              <h3 className='nav-video-header'>эксклюзив</h3>
+              <div className='nav-video-description'>Используйте уникальный контент в своих целях</div>
+            </div>
+            <div className='video-nav-element'>
+              <div className='img-video-nav'>
+                <div className='play-button'/>
+              </div>
+              <h3 className='nav-video-header'>высокое качество</h3>
+              <div className='nav-video-description'>Используйте уникальный контент в своих целях</div>
+            </div>
+          </nav>
+        </div>
+
+        <div className='more-block'>
+          <div className='more-block-text'>
+            <div>
+              <h2 className='more-block-header'>Создавайте свои сайты в пару кликов</h2>
+              <div className='more-block-description'>
+                Мы создали новый продукт, который поможет дизайнерам, разработчикам и небольшим стартапам создать свой сайт быстро и просто.
+              </div>
+              <div className='round-button pink-button'>Подробнее</div>
+            </div>
+            <div className='technologies'>
+              <div className='tech'>
+                <img src={ bootstrap } className='tech-pic' alt='bootstrap'/>
+                <h3 className='tech-header'>Bootstrap 4</h3>
+                <div className='tech-description'>
+                  HTML верстка основана на одном из самых распространенных и надежных фреймворков
+                </div>
+              </div>
+              <div className='tech'>
+                <img src={ html5 } className='tech-pic' alt='html5'/>
+                <h3 className='tech-header'>HTML5 & CSS3</h3>
+                <div className='tech-description'>
+                  Для достижения наилучшего результата мы использовали только проверенные временем технологии.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='more-block-pic'/>
+        </div>
+
+        <div className='spend-money'>
+          <div className='money-description'>
+            У нас есть два тарифа
+          </div>
+          <div className='switch-container'>
+            <div>Месяц</div>
+
+            <label className='switch'>
+              <input id='time' type='checkbox' onClick={() => { setTime(!time) }}/>
+              <div className='slider'/>
+            </label>
+            
+            <div>Год</div>
+          </div>
+          <div className='choose-tariff'>
+            <div className='tariff'>
+              <div className='tariff-pic tariff-pic-low'/>
+              <div className='tariff-description'>
+                <div>
+                  <div className='tariff-type'>
+                    <div className='tariff-name'>
+                      Оптимальный
+                    </div>
+                    <div className='tariff-price'>
+                      500₽
+                    </div>
+                  </div>
+                  <div className='tariff-text'>
+                    Редактируйте фото и видео в режиме онлайн. Создавайте одностраничные сайты из готовых компонентов.
+                  </div>
+                </div>
+                <div className='round-button choosen-pay-button pay-button' onClick={showForm}>Начать бесплатно</div>
+              </div>
+            </div>
+            <div className='tariff'>
+              <div className='tariff-pic tariff-pic-high'/>
+              <div className='tariff-description'>
+                <div>
+                  <div className='tariff-type'>
+                    <div className='tariff-name'>
+                      Профи
+                    </div>
+                    <div className='tariff-price'>
+                      1000₽
+                    </div>
+                  </div>
+                  <div className='tariff-text'>
+                    Редактируйте фото и видео в режиме онлайн. Создавайте одностраничные и многостраничные сайты  из готовых компонентов.
+                  </div>
+                </div>
+                <div className='round-button pay-button'>Начать бесплатно</div>
+              </div>
+            </div>
+          </div> 
+        </div>
+        <SendForm active={formVisibility} setActive={setFormVisibility} time={time}/>
+      </main>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/SendForm.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import '../styles/SendForm.css'
+
+export interface FormProps{
+    active: boolean;
+    setActive: any;
+    time: boolean;
+}
+
+export const SendForm = ( { active, setActive, time }: FormProps ) => {
+    const [gratitude, setGratitude] = useState(false);
+
+    function validationForm(e: any){
+        const form = e.target;
+        e.preventDefault();
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i.test(form[0].value)){
+            console.log("Email is incorrect!");
+            return;
+        }
+        if (/^\s*$/.test(form[1].value)){
+            console.log("Name is incorrect!");
+            return;
+        }
+        if (!/^\+?(\d{1,3})?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/i.test(form[2].value)){
+            console.log("Number of phone is incorrect!");
+            return;
+        }
+        if (!form[4].checked){
+            console.log("Checkbox is incorrect!");
+            return;
+        }
+
+        let obj = {email: form[0].value, name: form[1].value, phone: form[2].value, time: time?"year":"month"};
+        console.log(obj);
+        
+        for(let i = 0; i < 3; i++){
+            form[i].value = '';
+        }
+        form[4].checked = false;
+
+        setGratitude(true);
+    }
+
+    let content;
+    if(!gratitude){
+        content = 
+                    <form className="form" onClick={ e => e.stopPropagation() } onSubmit={ validationForm }>
+                        <div className='form-header'>
+                            Оставить заявку
+                        </div>
+                        <div className='text form-description'>
+                            Менеджеры перезвонят или напишут в течение дня.<br/>
+                            Поможем определиться и протестировать продукт.<br/>
+                            Без спама и навязчивости
+                        </div>
+                        <input className='input' type='email' placeholder='Email'/>
+                        <input className='input' placeholder='Иван Иванов'/>
+                        <input className='input' placeholder='Телефон'/>
+                        <button className='send-button' type="submit">Отправить</button>
+                        <div className='check-container'>
+                            <input id='check' className='checkbox' type="checkbox"/>
+                            <label htmlFor='check'/>
+                            <div className='check-text'>
+                                Я соглашаюсь на обработку персональных данных и присоединяюсь к регламенту СБИС
+                            </div>
+                        </div>
+                    </form>
+    }
+    else{
+        content = 
+                    <div className='form' onClick={() => { setActive(false) }}>
+                        <div className='form-header'>
+                            Спасибо за заявку
+                        </div>
+                        <div className='text'>
+                            Наши менеджеры свяжутся с вами в течении 5 минут
+                        </div>
+                    </div>
+    }
+
+    return(
+        <div className={active ? "form-screen" : "form-screen active"} onClick={() => { 
+                setGratitude(false);
+                setActive(false)
+            }}>
+            { content }            
+        </div>
+    )
+}
